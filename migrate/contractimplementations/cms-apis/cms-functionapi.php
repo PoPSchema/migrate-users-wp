8474a9d7bb8c2c1c12066217374474d006e09f5c
Leo <losoviz@gmail.com>
2019-11-06 19:52:14 +0800

    Filter users by email

--- migrate/contractimplementations/cms-apis/cms-functionapi.php
+++ migrate/contractimplementations/cms-apis/cms-functionapi.php
@@ -68,9 +68,26 @@ class FunctionAPI extends \PoP\Users\FunctionAPI_Base
             // Same param name, so do nothing
         }
         if (isset($query['limit'])) {
-
             $query['number'] = $query['limit'];
             unset($query['limit']);
+        }
+        // Limit users which have an email appearing on the input
+        // WordPress does not allow to search by many email addresses, only 1!
+        // Then we implement a hack to allow for it:
+        // 1. Set field "search", as expected
+        // 2. Add a hook which will modify the SQL query
+        // 3. Execute query
+        // 4. Remove hook
+        $filterByEmails = false;
+        if (isset($query['emails'])) {
+            $emails = $query['emails'];
+            // This works for either 1 or many emails
+            $query['search'] = implode(',', $emails);
+            // But if there's more than 1 email, we must modify the SQL query with a hook
+            if (count($emails) > 1) {
+                add_action('pre_user_query', [$this, 'enableMultipleEmails']);
+                $filterByEmails = true;
+            }
         }
 
         $query = HooksAPIFacade::getInstance()->applyFilters(
@@ -78,7 +95,40 @@ class FunctionAPI extends \PoP\Users\FunctionAPI_Base
             $query,
             $options
         );
-        return get_users($query);
+        $ret = get_users($query);
+        // Remove the hook
+        if ($filterByEmails) {
+            remove_action('pre_user_query', [$this, 'enableMultipleEmails']);
+        }
+        return $ret;
+    }
+    /**
+     * Modify the SQL query, replacing searching for a single email (with SQL operation "=") to multiple ones (with SQL operation "IN")
+     *
+     * @param [type] $query
+     * @return void
+     */
+    public function enableMultipleEmails($query) {
+        $qv =& $query->query_vars;
+        // var_dump('adentro', $qv);
+        if (isset($qv['search'])) {
+            $search = trim( $qv['search'] );
+            // Validate it has no wildcards, it's email (because there's a "@") and there's more than one (because there's ",")
+            $leading_wild = (ltrim($search, '*') != $search);
+            $trailing_wild = (rtrim($search, '*') != $search);
+            if (!$leading_wild && !$trailing_wild && false !== strpos($search, '@') && false !== strpos($search, ',')) {
+                // global $wpdb;
+                // $searches = [$wpdb->prepare( "user_email IN (%s)", $search )];
+                // Replace the query
+                $emails = explode(',', $search);
+                $searches = [sprintf("user_email IN (%s)", "'".implode("','", $emails)."'")];
+                $replace = $query->get_search_sql( $search, ['user_email'], false );
+                $replacement = ' AND (' .implode(' OR ', $searches).')';
+                // var_dump('before', $replace, $replacement, $query->query_where);
+                $query->query_where = str_replace($replace, $replacement, $query->query_where);
+                // var_dump('after', $query->query_where);
+            }
+        }
     }
     public function getUserDisplayName($user_id)
     {
